0fb887a59725bcc39ff1bdea484423470487b49e
Francois Margot <fmargot@andrew.cmu.edu>
2006-09-10 13:29:53 +0000

    Fix bug for solvers not supporting getDblParam() for OsiDualObjectiveLimit

--- Cgl/src/CglProbing/CglProbing.cpp
+++ Cgl/src/CglProbing/CglProbing.cpp
@@ -1410,7 +1410,10 @@ int CglProbing::gutsOfGenerateCuts(const OsiSolverInterface & si,
 
   // get branch and bound cutoff
   double cutoff;
-  si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  bool cutoff_available = si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  if (!cutoff_available) { // cut off isn't set or isn't valid
+    cutoff = si.getInfinity();
+  }
   cutoff *= si.getObjSense();
   if (fabs(cutoff)>1.0e30)
     assert (cutoff>1.0e30);
@@ -1455,7 +1458,10 @@ int CglProbing::gutsOfGenerateCuts(const OsiSolverInterface & si,
     const double * colsol =si.getColSolution();
     double direction = si.getObjSense();
     double cutoff;
-    si.getDblParam(OsiDualObjectiveLimit,cutoff);
+    bool cutoff_available = si.getDblParam(OsiDualObjectiveLimit,cutoff);
+    if (!cutoff_available) { // cut off isn't set or isn't valid
+      cutoff = si.getInfinity();
+    }
     cutoff *= direction;
     if (fabs(cutoff)>1.0e30)
       assert (cutoff>1.0e30);
@@ -2472,7 +2478,10 @@ int CglProbing::probe( const OsiSolverInterface & si,
   int ipass=0,nfixed=-1;
 
   double cutoff;
-  si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  bool cutoff_available = si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  if (!cutoff_available) { // cut off isn't set or isn't valid
+    cutoff = si.getInfinity();
+  }
   cutoff *= direction;
   if (fabs(cutoff)>1.0e30)
     assert (cutoff>1.0e30);
@@ -4030,7 +4039,10 @@ int CglProbing::probeCliques( const OsiSolverInterface & si,
   int ipass=0,nfixed=-1;
 
   double cutoff;
-  si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  bool cutoff_available = si.getDblParam(OsiDualObjectiveLimit,cutoff);
+  if (!cutoff_available) { // cut off isn't set or isn't valid
+    cutoff = si.getInfinity();
+  }
   cutoff *= si.getObjSense();
   if (fabs(cutoff)>1.0e30)
     assert (cutoff>1.0e30);
