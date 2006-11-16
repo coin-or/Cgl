a494f9e84aa6b52224fb04bd48128447329a091d
Pierre Bonami <pierre.bonami@gmail.com>
2006-11-16 20:33:18 +0000

    Fix error found by Francois in validator

--- Cgl/src/CglLandP/CglLandPValidator.cpp
+++ Cgl/src/CglLandP/CglLandPValidator.cpp
@@ -52,16 +52,16 @@ CglValidator::cleanCut(OsiRowCut & aCut, const double * solCut, const OsiSolverI
       if(val==0) {offset++; continue;}    
 
       int & iCol = indices[i];
-      if(elems[i]>0. && colLower[iCol] > -10000.)
-	{
-	  offset++;
-	  rhs -= elems[i] * colLower[iCol];
-	  elems[i]=0;
-	}
-      else if (elems[i]<0. && colUpper[iCol] < 10000.)
+      if(elems[i]>0. && colUpper[iCol] > -10000.)
 	{
 	  offset++;
 	  rhs -= elems[i] * colUpper[iCol];
+	  elems[i]=0;
+	}
+      else if (elems[i]<0. && colLower[iCol] < 10000.)
+	{
+	  offset++;
+	  rhs -= elems[i] * colLower[iCol];
 	  elems[i]=0.;
 	}
       else
@@ -199,16 +199,16 @@ CglValidator::cleanCut2(OsiRowCut & aCut, const double * solCut, const OsiSolver
 	    {
  	      if(val< veryTiny) {offset++; continue;}
  	      int & iCol = indices[i];
- 	      if(elems[i]>0. && colLower[iCol] > -1000.)
-        {
-          offset++;
-          rhs -= elems[i] * colLower[iCol];
-          elems[i]=0;
-        }
-      else if (elems[i]<0. && colUpper[iCol] < 1000.)
+ 	      if(elems[i]>0. && colUpper[iCol] > -1000.)
         {
           offset++;
           rhs -= elems[i] * colUpper[iCol];
+          elems[i]=0;
+        }
+      else if (elems[i]<0. && colLower[iCol] < 1000.)
+        {
+          offset++;
+          rhs -= elems[i] * colLower[iCol];
           elems[i]=0.;
         }
  	      else
