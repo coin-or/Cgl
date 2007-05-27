f62262efb1649a35063787b97a9598867a63614d
Francois Margot <fmargot@andrew.cmu.edu>
2007-05-27 13:20:30 +0000

    Modify test to work with egout.mps

--- Cgl/src/CglFlowCover/CglFlowCoverTest.cpp
+++ Cgl/src/CglFlowCover/CglFlowCoverTest.cpp
@@ -18,7 +18,7 @@
 #include <cassert>
 #include <iostream>
 
-#include "CoinPackedMatrix.hpp"
+//#include "CoinPackedMatrix.hpp"
 #include "CglFlowCover.hpp"
 
 //--------------------------------------------------------------------------
@@ -28,8 +28,6 @@ CglFlowCoverUnitTest(
   const OsiSolverInterface * baseSiP,
   const std::string mpsDir )
 {
-  std::cout << "mpsDir = " << mpsDir << std::endl;
-  
   // Test default constructor
   {
     CglFlowCover aGenerator;
@@ -53,93 +51,111 @@ CglFlowCoverUnitTest(
 
     OsiSolverInterface  * siP = baseSiP->clone();
     
-    int i;
     int nRowCuts;
-    
-    std::cout << "Before assert " << std::endl;
-
-    std::string fn(mpsDir+"blend2");
-
-    std::cout << "After assert " << std::endl; 
-    std::cout << "fn " << fn <<std::endl;
-
-    siP->readMps(fn.c_str(), "mps");
+    std::string fn(mpsDir+"egout");
+    std::string fn2 = mpsDir+"egout.mps";
+    FILE *in_f = fopen(fn2.c_str(), "r");
+    if(in_f == NULL) {
+      std::cout<<"Can not open file "<<fn2<<std::endl<<"Skip test of CglFlowCover::generateCuts()"<<std::endl;
+    }
+    else {
+      fclose(in_f);
+      siP->readMps(fn.c_str(), "mps");
  
-    // Check the preprocess
-    test.flowPreprocess(*siP);
-    test.printVubs(std::cout);
+      // Check the preprocess
+      test.flowPreprocess(*siP);
 
-    // Test generating cuts
-    siP->initialSolve();
-    test.generateCuts(*siP, osicuts1);
-    nRowCuts = osicuts1.sizeRowCuts();
-    std::cout<<"There are " << nRowCuts << " flow cuts" << std::endl;
-    
-    for (i = 0; i < nRowCuts; i++){
-      OsiRowCut rcut;
-      CoinPackedVector rpv;
-      const double* colsol = siP->getColSolution();
-      rcut = osicuts1.rowCut(i);
-      rpv = rcut.row();
-      const int n = rpv.getNumElements();
-      const int* indices = rpv.getIndices();
-      double* elements = rpv.getElements();
-      double sum2 = 0.0;
-      int k = 0;
-      double lb = rcut.lb();
-      double ub = rcut.ub();
-      for (k = 0; k < n; ++k){
-	int column = indices[k];
-	sum2 += colsol[column] * elements[k];
-      }
-      if (sum2 > ub + 1.0e-7 ||sum2 < lb - 1.0e-7) {
-	std::cout << "Cut " << i <<" lb " << lb << " solution " << sum2 
-		  <<" ub " << ub << std::endl;
+#ifdef CGL_DEBUG
+      test.printVubs(std::cout);
+#endif
+
+      // Test generating cuts
+      siP->initialSolve();
+      double lpRelax = siP->getObjValue();
+      test.generateCuts(*siP, osicuts1);
+      nRowCuts = osicuts1.sizeRowCuts();
+
+#ifdef CGL_DEBUG      
+      std::cout<<"There are " << nRowCuts << " flow cuts" << std::endl;
+      int i;
+      for (i = 0; i < nRowCuts; i++){
+	OsiRowCut rcut;
+	CoinPackedVector rpv;
+	const double* colsol = siP->getColSolution();
+	rcut = osicuts1.rowCut(i);
+	rpv = rcut.row();
+	const int n = rpv.getNumElements();
+	const int* indices = rpv.getIndices();
+	double* elements = rpv.getElements();
+	double sum2 = 0.0;
+	int k = 0;
+	double lb = rcut.lb();
+	double ub = rcut.ub();
 	for (k = 0; k < n; ++k){
 	  int column = indices[k];
-	  std::cout << "(col=" << column << ", el = " << elements[k] 
-		    << ", sol = " << colsol[column] << ") ";
+	  sum2 += colsol[column] * elements[k];
 	}
-	std::cout <<std::endl;
+	if (sum2 > ub + 1.0e-7 ||sum2 < lb - 1.0e-7) {
+	  std::cout << "Cut " << i <<" lb " << lb << " solution " << sum2 
+		    <<" ub " << ub << std::endl;
+	  for (k = 0; k < n; ++k){
+	    int column = indices[k];
+	    std::cout << "(col=" << column << ", el = " << elements[k] 
+		      << ", sol = " << colsol[column] << ") ";
+	  }
+	  std::cout <<std::endl;
+	}
       }
-    }
+#endif
 
     // Test generating cuts again
-    OsiCuts osicuts2;
-    test.generateCuts(*siP, osicuts2);
-    OsiSolverInterface::ApplyCutsReturnCode rc = siP->applyCuts(osicuts2);
-    siP->resolve();
-    nRowCuts = osicuts2.sizeRowCuts();
-    std::cout<<"There are " << nRowCuts << " flow cuts" << std::endl;
-    for (i = 0; i < nRowCuts; i++){
-      OsiRowCut rcut;
-      CoinPackedVector rpv;
-      const double* colsol = siP->getColSolution();
-      rcut = osicuts2.rowCut(i);
-      rpv = rcut.row();
-      const int n = rpv.getNumElements();
-      const int* indices = rpv.getIndices();
-      double* elements = rpv.getElements();
-      double sum2 = 0.0;
-      int k = 0;
-      double lb = rcut.lb();
-      double ub = rcut.ub();
-      for (k = 0; k < n; ++k){
-	int column = indices[k];
-	sum2 += colsol[column] * elements[k];
-      }
-      if (sum2 > ub + 1.0e-7 ||sum2 < lb - 1.0e-7) {
-	std::cout << "Cut " << i <<" lb " << lb << " solution " << sum2 
-		  <<" ub " << ub << std::endl;
+      OsiCuts osicuts2;
+      test.generateCuts(*siP, osicuts2);
+      OsiSolverInterface::ApplyCutsReturnCode rc = siP->applyCuts(osicuts2);
+      siP->resolve();
+      nRowCuts = osicuts2.sizeRowCuts();
+      std::cout<<"There are " << nRowCuts << " flow cuts" << std::endl;
+
+#ifdef CGL_DEBUG
+      for (i = 0; i < nRowCuts; i++){
+	OsiRowCut rcut;
+	CoinPackedVector rpv;
+	const double* colsol = siP->getColSolution();
+	rcut = osicuts2.rowCut(i);
+	rpv = rcut.row();
+	const int n = rpv.getNumElements();
+	const int* indices = rpv.getIndices();
+	double* elements = rpv.getElements();
+	double sum2 = 0.0;
+	int k = 0;
+	double lb = rcut.lb();
+	double ub = rcut.ub();
 	for (k = 0; k < n; ++k){
 	  int column = indices[k];
-	  std::cout << "(col=" << column << ", el = " << elements[k] 
-		    << ", sol = " << colsol[column] << ") ";
+	  sum2 += colsol[column] * elements[k];
 	}
-	std::cout <<std::endl;
+	if (sum2 > ub + 1.0e-7 ||sum2 < lb - 1.0e-7) {
+	  std::cout << "Cut " << i <<" lb " << lb << " solution " << sum2 
+		    <<" ub " << ub << std::endl;
+	  for (k = 0; k < n; ++k){
+	    int column = indices[k];
+	    std::cout << "(col=" << column << ", el = " << elements[k] 
+		      << ", sol = " << colsol[column] << ") ";
+	  }
+	  std::cout <<std::endl;
+	}
       }
+#endif
+      assert(osicuts2.sizeRowCuts() > 0);
+      rc = siP->applyCuts(osicuts2);
+      
+      siP->resolve();
+      
+      double lpRelaxAfter= siP->getObjValue(); 
+      std::cout<<"Initial LP value: "<<lpRelax<<std::endl;
+      std::cout<<"LP value with cuts: "<<lpRelaxAfter<<std::endl;
+      assert( lpRelax < lpRelaxAfter );
     }
-
     delete siP;
   }
 }
