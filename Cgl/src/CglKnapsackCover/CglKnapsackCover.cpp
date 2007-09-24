23c35b42629034e32f7c4052d3f6ddd52cd2681f
Robin Lougee <rlougee@us.ibm.com>
2007-09-24 21:03:54 +0000

    corrected variable name for ticket #12

--- Cgl/src/CglKnapsackCover/CglKnapsackCover.cpp
+++ Cgl/src/CglKnapsackCover/CglKnapsackCover.cpp
@@ -1912,7 +1912,7 @@ CglKnapsackCover::findJohnAndEllisCover(
   }
 
   // If no minimal cover was found, pack it in   
-  if (!gotCover || cover.getNumElements() < 2) {
+  if (!gotCover || fracCover.getNumElements() < 2) {
     return -1;
   }
   
