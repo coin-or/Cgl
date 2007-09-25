b8aa23f21cc4859bd7d68c61c8c5331a88ff6c6a
John Forrest <john.forrest@fastercoin.com>
2007-09-25 15:03:18 +0000

    for aggregation

--- Cgl/src/CglMessage.cpp
+++ Cgl/src/CglMessage.cpp
@@ -22,8 +22,10 @@ static Cgl_message us_english[]=
   {CGL_PROCESS_SOS1,5,1,"%d SOS with %d members"},
   {CGL_PROCESS_SOS2,6,1,"%d SOS (%d members out of %d) with %d overlaps - too much overlap or too many others"},
   {CGL_UNBOUNDED,0,7,"Continuous relaxation is unbounded!"},
-  {CGL_ELEMENTS_CHANGED1,8,2,"%d elements changed"},
-  {CGL_ELEMENTS_CHANGED2,9,3,"element in row %d for column %d changed from %g to %g"},
+  {CGL_ELEMENTS_CHANGED1,9,2,"%d elements changed"},
+  {CGL_ELEMENTS_CHANGED2,10,3,"element in row %d for column %d changed from %g to %g"},
+  {CGL_MADE_INTEGER,11,1,"%d variables made integer"},
+  {CGL_ADDED_INTEGERS,12,1,"Added %d variables (from %d rows) with %d elements\n"},
   {CGL_DUMMY_END,999999,0,""}
 };
 /* Constructor */
