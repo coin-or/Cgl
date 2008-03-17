2e5ea99c92c2e816248eb8cbc93e1b092f6f3687
Robin Lougee <rlougee@us.ibm.com>
2008-03-17 19:44:28 +0000

    turned lifting on by default

--- Cgl/src/CglFlowCover/CglFlowCover.cpp
+++ Cgl/src/CglFlowCover/CglFlowCover.cpp
@@ -910,7 +910,7 @@ CglFlowCover::generateOneFlowCut( const OsiSolverInterface & si,
 	}
     }
 
-#if 0 // LIFTING?
+#if 1 // LIFTING?
     double estY, estX;
     double movement = 0.0;
     double dPrimePrime = temp + cutRHS; 
