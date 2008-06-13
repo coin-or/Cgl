7e0edc9d948bf19e21acb4806c24ad22e2d36ee6
Robin Lougee <rlougee@us.ibm.com>
2008-06-13 14:53:50 +0000

    Fix for ticket #26. Lifting turned back on.

--- Cgl/src/CglFlowCover/CglFlowCover.cpp
+++ Cgl/src/CglFlowCover/CglFlowCover.cpp
@@ -911,7 +911,7 @@ CglFlowCover::generateOneFlowCut( const OsiSolverInterface & si,
 	}
     }
 
-#if 0 // LIFTING?
+#if 1 // LIFTING?
     double estY, estX;
     double movement = 0.0;
     double dPrimePrime = temp + cutRHS; 
@@ -951,7 +951,7 @@ CglFlowCover::generateOneFlowCut( const OsiSolverInterface & si,
 		       colInd[i], movement);
 #endif
 		lifted = true;
-		yCoef[i] = -movement;
+		xCoef[i] = -movement;
 		cutRHS -= movement;
 	    }
 	    else {
