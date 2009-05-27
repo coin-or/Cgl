43f80c5d80f7fc72ed0e54d5d708957e0b498af9
Robin Lougee <rlougee@us.ibm.com>
2009-05-27 19:52:01 +0000

    fixing tolerance issue in lifting

--- Cgl/src/CglFlowCover/CglFlowCover.hpp
+++ Cgl/src/CglFlowCover/CglFlowCover.hpp
@@ -36,13 +36,7 @@ enum CglFlowColType {
     CGLFLOW_COL_BINPOS
 };
 
-/** This enumerative constant describes the various stati of vars in 
-    determining the cover.*/
 enum CglFlowColStatus{
-    /** The column is a prime candidate. */
-    CGLFLOW_COL_PRIME,
-    /** The column is a secondary candidate. */
-    CGLFLOW_COL_SECONDARY
 };
 
 /** This enumerative constant describes the various stati of vars in 
@@ -59,7 +53,13 @@ enum CglFlowColCut{
     /** The column is decided to be in L-. */
     CGLFLOW_COL_INLMINDONE,
     /** The column is in L--.*/
-    CGLFLOW_COL_INLMINMIN
+    CGLFLOW_COL_INLMINMIN,
+    /** This enumerative constant describes the various stati of vars in 
+                   determining the cover.*/
+    /** The column is a prime candidate. */
+    CGLFLOW_COL_PRIME,
+    /** The column is a secondary candidate. */
+    CGLFLOW_COL_SECONDARY
 };
 
 /** This enumerative constant describes the various row types.*/
