35202e5f8a6728c3ee380bec538c6f5cfc691dd0
Lou Hafer <lou@sfu.ca>
2011-02-18 15:44:21 +0000

    Clean up doxydoc.

--- src/CglDuplicateRow/CglDuplicateRow.hpp
+++ src/CglDuplicateRow/CglDuplicateRow.hpp
@@ -11,118 +11,104 @@
 #include "CglCutGenerator.hpp"
 class CglStored;
 
-/** DuplicateRow Cut Generator Class */
+/*! \brief DuplicateRow Cut Generator Class
+
+  This is a very simple minded idea but I (JJF) am using it in a project
+  so thought I might as well add it.  It should really be called before
+  first solve and I may modify CBC to allow for that.
+
+  This is designed for problems with few rows and many integer variables
+  where the rhs are <= or == and all coefficients and rhs are small integers.
+
+  If effective rhs is K then we can fix all variables with coefficients > K
+  to their lower bounds (effective rhs just means original with variables with
+  nonzero lower bounds subtracted out).
+
+  If one row is a subset of another and the effective rhs are same we can
+  fix some variables and then the two rows are identical.
+
+  The generator marks identical rows so can be taken out in solve.
+*/
 class CglDuplicateRow : public CglCutGenerator {
  
 public:
-    
   
-  /**@name Generate Cuts */
+  /*! \name Generate Cuts */
   //@{
-  /** Fix variables and find duplicate/dominated rows for the model of the 
-      solver interface, si.
+  /// Look for duplicate/dominated rows and variables that can be fixed.
+  virtual void generateCuts(const OsiSolverInterface & si, OsiCuts & cs,
+			    const CglTreeInfo info = CglTreeInfo()) const;
 
-      This is a very simple minded idea but I (JJF) am using it in a project so thought
-      I might as well add it.  It should really be called before first solve and I may
-      modify CBC to allow for that.
+  /*! \brief Fix variables and find duplicate/dominated rows
 
-      This is designed for problems with few rows and many integer variables where the rhs
-      are <= or == and all coefficients and rhs are small integers.
-
-      If effective rhs is K then we can fix all variables with coefficients > K to their lower bounds
-      (effective rhs just means original with variables with nonzero lower bounds subtracted out).
-
-      If one row is a subset of another and the effective rhs are same we can fix some variables
-      and then the two rows are identical.
-
-      The generator marks identical rows so can be taken out in solve
-  */
-  virtual void generateCuts( const OsiSolverInterface & si, OsiCuts & cs,
-			     const CglTreeInfo info = CglTreeInfo()) const;
-private:
-  /// Does work for modes 1,2
-  void generateCuts12( const OsiSolverInterface & si, OsiCuts & cs,
-		       const CglTreeInfo info = CglTreeInfo()) const;
-  /// Does work for mode 4
-  void generateCuts4( const OsiSolverInterface & si, OsiCuts & cs,
-		       const CglTreeInfo info = CglTreeInfo()) const;
-  /// Does work for mode 8
-  void generateCuts8( const OsiSolverInterface & si, OsiCuts & cs,
-		       const CglTreeInfo info = CglTreeInfo()) const;
-public:
-  /** Fix variables and find duplicate/dominated rows for the model of the 
-      solver interface, si.
-
-      This is a very simple minded idea but I (JJF) am using it in a project so thought
-      I might as well add it.  It should really be called before first solve and I may
-      modify CBC to allow for that.
-
-      This is designed for problems with few rows and many integer variables where the rhs
-      are <= or == and all coefficients and rhs are small integers.
-
-      If effective rhs is K then we can fix all variables with coefficients > K to their lower bounds
-      (effective rhs just means original with variables with nonzero lower bounds subtracted out).
-
-      If one row is a subset of another and the effective rhs are same we can fix some variables
-      and then the two rows are identical.
-
-      This version does deletions and fixings and may return stored cuts for
-      dominated columns 
+    This version performs the deletions and fixings and may return stored
+    cuts for dominated columns 
   */
   CglStored * outDuplicates( OsiSolverInterface * solver);
 
   //@}
 
-  /**@name Get information on size of problem */
+public:
+
+  /**@name Get problem information */
   //@{
-  /// Get duplicate row list, -1 means still in, -2 means out (all fixed), k>= means same as row k 
-  inline const int * duplicate() const
-  { return duplicate_;}
+  /*! \brief Get the duplicate row list.
+  
+    Entries in the list are coded as:
+    - -1: still in
+    - -2: out (all fixed)
+    - -3: not clique (mode 0x08 only?)
+    -  k >= 0: same as row k 
+  */
+  inline const int * duplicate() const { return duplicate_;}
+
   /// Size of dynamic program
-  inline int sizeDynamic() const
-  { return sizeDynamic_;}
+  inline int sizeDynamic() const { return sizeDynamic_;}
+
   /// Number of rows in original problem
-  inline int numberOriginalRows() const
-  { return matrix_.getNumRows();}
+  inline int numberOriginalRows() const { return matrix_.getNumRows();}
   //@}
 
-  /**@name Get information on size of problem */
+  /*! \name Control methods */
   //@{
-  /// logLevel
-  inline int logLevel() const
-  { return logLevel_;}
-  inline void setLogLevel(int value)
-  { logLevel_ = value;}
-  //@}
 
-
-  /**@name We only check for duplicates amongst rows with effective rhs <= this */
-  //@{
-  /// Get
-  inline int maximumRhs() const
-  { return maximumRhs_;}
-  /// Set
-  inline void setMaximumRhs(int value)
-  { maximumRhs_=value;}
-  //@}
-
-  /**@name We only check for dominated amongst groups of columns whose size <= this */
-  //@{
-  /// Get
-  inline int maximumDominated() const
-  { return maximumDominated_;}
-  /// Set
-  inline void setMaximumDominated(int value)
-  { maximumDominated_=value;}
-  //@}
-  /**@name gets and sets */
-  //@{
   /// Get mode
-  inline int mode() const
-  { return mode_;}
-  /// Set mode
-  inline void setMode(int value)
-  { mode_=value;}
+  inline int mode() const { return mode_;}
+
+  /*! \brief Set mode
+
+    - 0x01: check for duplicate rows
+    - 0x02: check for duplicate columns
+    - 0x04: undocumented
+    - 0x08: undocumented
+  */
+  inline void setMode(int value) { mode_=value;}
+
+  /// Get right-hand-side magnitude limit
+  inline int maximumRhs() const { return maximumRhs_;}
+
+  /*! \brief Set right-hand-side magnitude limit
+
+    We only check for duplicates amongst rows where the magnitude of the
+    effective rhs is less than the given value.
+  */
+  inline void setMaximumRhs(int value) { maximumRhs_=value;}
+
+  /// Get column size limit
+  inline int maximumDominated() const { return maximumDominated_;}
+
+  /*! \brief Set column size limit
+
+    We only check for dominated columns amongst groups of columns whose size
+    is less than the given value.
+  */
+  inline void setMaximumDominated(int value) { maximumDominated_=value;}
+
+  /// Get log level
+  inline int logLevel() const { return logLevel_;}
+
+  /// Set log level
+  inline void setLogLevel(int value) { logLevel_ = value;}
   //@}
 
   /**@name Constructors and destructors */
@@ -184,6 +170,21 @@ protected:
   int mode_;
   /// Controls print out
   int logLevel_;
+  //@}
+
+private:
+
+  /*! \name Cut generation worker methods */
+  //@{
+  /// Does work for modes 1,2
+  void generateCuts12( const OsiSolverInterface & si, OsiCuts & cs,
+		       const CglTreeInfo info = CglTreeInfo()) const;
+  /// Does work for mode 4
+  void generateCuts4( const OsiSolverInterface & si, OsiCuts & cs,
+		       const CglTreeInfo info = CglTreeInfo()) const;
+  /// Does work for mode 8
+  void generateCuts8( const OsiSolverInterface & si, OsiCuts & cs,
+		       const CglTreeInfo info = CglTreeInfo()) const;
   //@}
 };
 #endif
