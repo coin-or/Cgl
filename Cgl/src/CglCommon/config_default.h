3acf37d2dd3f1fdbe016bbcec7b7757212a69d89
Stefan Vigerske <svigerske@gams.com>
2020-01-14 13:58:11 +0000

    add CGLLIB_EXPORT to config default h's

--- Cgl/src/CglCommon/config_default.h
+++ Cgl/src/CglCommon/config_default.h
@@ -2,6 +2,16 @@
 /* include the COIN-OR-wide system specific configure header */
 #include "configall_system.h"
 
+/* this needs to come before the include of config_coinutils_default.h */
+#ifndef CGLLIB_EXPORT
+#ifdef _WIN32
+/* assuming we build a CoinUtils DLL */
+#define CGLLIB_EXPORT __declspec(dllexport)
+#else
+#define CGLLIB_EXPORT
+#endif
+#endif
+
 /* include the public project specific macros */
 #include "config_cgl_default.h"
 
@@ -10,14 +20,6 @@
 /*    These are only in effect in a setting that doesn't use configure     */
 /***************************************************************************/
 
-/* Define to the debug sanity check level (0 is no test) */
-#define COIN_CGL_CHECKLEVEL 0
-
-/* Define to the debug verbosity level (0 is no output) */
-#define COIN_CGL_VERBOSITY 0
-
-/* Define to 1 if the Clp package is used */
-#define COIN_HAS_OSICLP 1
 
 /* Define to 1 if the CoinUtils package is used */
 #define COIN_HAS_COINUTILS 1
@@ -25,5 +27,9 @@
 /* Define to 1 if the Osi package is used */
 #define COIN_HAS_OSI 1
 
-/* Define to 1 if the Vol package is used */
-#define COIN_HAS_VOL 1
+/* Define to 1 if the Osi package is used */
+#define COIN_HAS_CLP 1
+
+/* Define to 1 if the Clp package is used */
+#define COIN_HAS_OSICLP 1
+
