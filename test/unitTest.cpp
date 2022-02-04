eef6ca0d69763e97bc8b5585d85a8ec4bd305788
Ted Ralphs <ted@lehigh.edu>
2022-02-04 09:43:37 -0500

    Change from <> to ""
    Recent version of gcc ignore the user-defined search path when looking for headers in <>. This is the way it's supposed to be, but I guess it wasn't enforced before. gcc finally refused to compile this.

--- test/unitTest.cpp
+++ test/unitTest.cpp
@@ -15,22 +15,22 @@
 #include <cstdlib>
 
 #ifdef CGL_HAS_OSICPX
-#include <OsiCpxSolverInterface.hpp>
+#include "OsiCpxSolverInterface.hpp"
 #endif
 #ifdef CGL_HAS_OSIXPR
-#include <OsiXprSolverInterface.hpp>
+#include "OsiXprSolverInterface.hpp"
 #endif
 #ifdef CGL_HAS_OSICLP
-#include <OsiClpSolverInterface.hpp>
+#include "OsiClpSolverInterface.hpp"
 #endif
 #ifdef CGL_HAS_OSIDYLP
-#include <OsiDylpSolverInterface.hpp>
+#include "OsiDylpSolverInterface.hpp"
 #endif
 #ifdef CGL_HAS_OSIGLPK
-#include <OsiGlpkSolverInterface.hpp>
+#include "OsiGlpkSolverInterface.hpp"
 #endif
 #ifdef CGL_HAS_OSIVOL
-#include <OsiVolSolverInterface.hpp>
+#include "OsiVolSolverInterface.hpp"
 #endif
 
 #include "CglSimpleRounding.hpp"
